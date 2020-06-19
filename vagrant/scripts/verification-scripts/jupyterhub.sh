2d0daff11d8c527f7a2d587960e19331eeb5b983
Paul Pacquing <dominicpacquing@gmail.com>
2020-06-19 13:44:26 +1000

    Update verification script

--- vagrant/scripts/verification-scripts/jupyterhub.sh
+++ vagrant/scripts/verification-scripts/jupyterhub.sh
@@ -1,9 +1,10 @@
-sudo useradd ppacquing
-echo "vagrant" | sudo passwd --stdin ppacquing
+sudo useradd paul.pacquing
+echo "vagrant" | sudo passwd --stdin paul.pacquing
 
 mkdir jupyterconfig
 cd jupyterconfig
 /usr/local/anaconda3/bin/jupyterhub --generate-config
 # Add spawner
+# vi jupyterhub_config.py
 # c.Spawner.cmd = '/usr/local/anaconda3/bin/jupyterhub-singleuser'
 /usr/local/anaconda3/bin/jupyterhub -f jupyterhub_config.py
--- vagrant/scripts/verification-scripts/rstudio.sh
+++ vagrant/scripts/verification-scripts/rstudio.sh
@@ -4,3 +4,7 @@ echo "vagrant" | sudo passwd --stdin ppacquing
 # Sys.getenv(c("SPARK_HOME", "R_HOME", "R_PAPERSIZE"))
 # library(sparklyr)
 # sc <- spark_connect(master = "local")
+
+spark-shell
+import org.apache.spark.sql.SparkSession
+val spark_new = SparkSession.builder.enableHiveSupport().getOrCreate
